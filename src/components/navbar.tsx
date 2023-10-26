8378ea236575315606d09297d6edd7d32f38ea32
jjxrry <114961129+jjxrry@users.noreply.github.com>
2023-10-25 17:19:39 -0700

    edit navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,9 +21,10 @@ export default function Navbar () {
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  {/* <span className="sr-only">Open main menu</span> */}
+                  <span className="sr-only">Open side menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
@@ -31,19 +32,8 @@ export default function Navbar () {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="block h-9 w-auto lg:hidden"
-                    src= { logo }
-                    alt="Portfolio Icon"
-                  />
-                  <img
-                    className="hidden h-9 w-auto lg:block"
-                    src= { logo }
-                    alt="Portfolio Icon"
-                  />
-                </div>
+
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
@@ -59,7 +49,7 @@ export default function Navbar () {
                         }}
                         className={classNames(
                           item.current ? 'text-white hover:bg-gray-700' : 'text-white hover:bg-gray-700',
-                          'rounded-md px-3 py-2 text-sm font-medium'
+                          'rounded-md px-3 py-2 text-md font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
@@ -71,7 +61,7 @@ export default function Navbar () {
               </div>
 
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
+                {/* github dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
