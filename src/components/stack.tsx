f04560b9f15d939ee600a35b9b6198d95d9f627e
jerry <114961129+jjxrry@users.noreply.github.com>
2024-08-04 14:46:45 -0500

    fix img responsiveness

--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -7,15 +7,15 @@ import mongoLogo from '../assets/mongodb.png'
 
 export default function Stack () {
     return(
-        <div className='flex flex-col lg:flex-row align-start justify-center lg:-mt-20' id='stack'>
-            <p className='text-white -mt-2 self-start lg:self-center mx-auto lg:mx-0'>my stack</p>
-            <div className='flex flex-wrap justify-center lg:justify-start lg:flex-row md:flex-col p-10 gap-4'>
-                <img className="w-auto h-9" src={ reactLogo }></img>
-                <img className="w-auto h-9" src={ tsLogo }></img>
+        <div className='flex flex-col justify-center lg:flex-row align-start lg:-mt-20' id='stack'>
+            <p className='self-start mx-auto -mt-2 text-white lg:self-center lg:mx-0'>my stack</p>
+            <div className='flex flex-wrap justify-center gap-4 p-10 lg:justify-start lg:flex-row sm:flex-row'>
+                <img className="w-12 h-9" src={ reactLogo }></img>
+                <img className="w-12 h-9" src={ tsLogo }></img>
                 <img className="w-12 h-9" src={ tailwindLogo }></img>
-                <img className="w-auto h-9" src={ nodeLogo }></img>
+                <img className="w-12 h-9" src={ nodeLogo }></img>
                 {/* <img className="w-auto h-10" src={ mongoLogo }></img> */}
-                <img className="w-auto h-9" src={ htmlLogo }></img>
+                <img className="w-12 h-9" src={ htmlLogo }></img>
             </div>
         </div>
     )
