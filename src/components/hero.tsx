5b4a550138a4e6fb380b9b36bc4958517af17370
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-08-26 11:13:01 -0700

    clean up

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,13 +4,16 @@ export default function Hero() {
 
   return (
     <div className="h-400" id='home'>
-      <div className="relative px-6 pt-14 lg:px-8">
+      <div className="relative px-6 pt-2 lg:px-8">
         <div className="mx-auto max-w-5xl py-32 sm:py-48 lg:py-56">
-            <div className="flex flex-col md:flex-row">
-                <img className="w-full md:w-1/2 h-auto transform -translate-y-8" src={ heroSplash }></img>
-                <p className="md:ml-6 mt-7 text-xl leading-9 text-gray-50">
-                Hello! My name is dot, or Jerry and I am learning to become a software engineer and web developer. 
-                I work mainly in JavaScript and Python to create cool things! 
+            <div className="flex flex-col">
+                {/* <img className="w-full md:w-1/2 h-auto transform -translate-y-8" src={ heroSplash }></img> */}
+                <p className="md:ml-6 mt-7 text-xl leading-9 text-gray-50 hero-wrap">
+                Hello! My name is Jerry and I am learning to become a software engineer and web developer.
+                I take pleasure in solving problems and getting the visuals just right.
+                <br></br>
+                I most recently helped with development of Apple's homepage as an intern of thier 2024 cohort!
+                <br></br>
                 I am currently pursuing my Master of Science in Computer Science at Northeastern University. I am always on the look out for SWE opportunities and would love to show you my work.
                 </p>
             </div>
--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -7,15 +7,15 @@ import mongoLogo from '../assets/mongodb.png'
 
 export default function Stack () {
     return(
-        <div className='flex flex-col justify-center lg:flex-row align-start lg:-mt-20' id='stack'>
+        <div className='flex flex-col justify-center align-start lg:-mt-20' id='stack'>
             <p className='self-start mx-auto -mt-2 text-white lg:self-center lg:mx-0'>my stack</p>
-            <div className='flex flex-wrap justify-center gap-4 p-10 lg:justify-start lg:flex-row sm:flex-row'>
-                <img className="w-12 h-9" src={ reactLogo }></img>
+            <div className='flex flex-wrap justify-center gap-4 p-10 sm:flex-row'>
+                <img className="w-10 h-9" src={ reactLogo }></img>
                 <img className="w-12 h-9" src={ tsLogo }></img>
-                <img className="w-12 h-9" src={ tailwindLogo }></img>
-                <img className="w-12 h-9" src={ nodeLogo }></img>
+                <img className="w-10 h-9" src={ tailwindLogo }></img>
+                <img className="w-10 h-9" src={ nodeLogo }></img>
                 {/* <img className="w-auto h-10" src={ mongoLogo }></img> */}
-                <img className="w-12 h-9" src={ htmlLogo }></img>
+                <img className="w-10 h-9" src={ htmlLogo }></img>
             </div>
         </div>
     )
